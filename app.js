363a2bbe05ad313131f88b15c6515fb306d8a5ec
HCasaes <hugocasaes@gmail.com>
2023-02-06 17:31:27 -0300

    update app return json

--- app.js
+++ app.js
@@ -2,10 +2,34 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
-app.get('/', function (req, res) {
-    res.send('GET request to homepage')
-  })
+    // params search
+    const langs = ["java", "python", "javascript", "css", "dotnet"];
+    const order = "desc";
+    const sort = "starts";
 
-  app.listen(port, () => {
+    let array_langs = [];
+
+    langs.map(async (lang) => {
+        let response =  await fetch(`https://api.github.com/search/repositories?q=${lang}&sort=${sort}&order=${order}&page=1&per_page=1`)
+        let data = await response.json()
+        array_langs.push(data)
+
+    })
+
+
+app.get('/', async function (req, res) {
+
+
+    res.json(array_langs)
+
+    // response = await response.json();
+    // res.status(200).json(response);
+    // res.json(response)
+
+});
+    
+
+
+app.listen(port, () => {
     console.log(`Now listening on port ${port}`);
 }); 
