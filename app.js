203c0337589ec0e05fb19d5065eb8edd63dde8e3
HCasaes <hugocasaes@gmail.com>
2023-02-08 22:35:22 -0300

    Atualização da API e front funcionando.

--- app.js
+++ app.js
@@ -2,31 +2,37 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
+// view engine ejs
+app.set('view engine', 'ejs')
+
+app.use(express.static(__dirname + '/public'));
+
     // params search
-    const langs = ["java", "python", "javascript", "css", "dotnet"];
+    const langs = ["php", "python", "javascript", "go", "dotnet"];
     const order = "desc";
     const sort = "stars";
 
     let array_langs = [];
 
     langs.map(async (lang) => {
-        let response =  await fetch(`https://api.github.com/search/repositories?q=${lang}&sort=${sort}&order=${order}&page=1&per_page=1`)
+        let response =  await fetch(`https://api.github.com/search/topics?q=${lang}&o=desc&s=stars`)
         let data = await response.json()
         array_langs.push(data)
 
     })
 
+    // navegador http://localhost:5000/
+    // terminal npm start
 
 app.get('/', async function (req, res) {
-
-
     res.json(array_langs)
-
-    // response = await response.json();
-    // res.status(200).json(response);
-    // res.json(response)
-
 });
+
+app.get('/home', (req , res) => {
+    res.render('home', {
+        array_langs
+    })
+})
     
 
 
